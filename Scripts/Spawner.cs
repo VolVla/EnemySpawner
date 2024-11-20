219e78baf3c663b17e754d8dcc5802c6d97fbf13
VolVla <98841869+VolVla@users.noreply.github.com>
2024-11-20 21:50:21 +0300

    Update Spawner.cs

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Spawner : MonoBehaviour
 {
@@ -10,10 +8,10 @@ public class Spawner : MonoBehaviour
     [SerializeField] private List<GameObject> _spawnPoint = new List<GameObject>();
     [SerializeField] private float _coolDown = 2f;
     [SerializeField] private bool _isActiveSpawner = true;
-    [SerializeField] private bool Forward = false;
-    [SerializeField] private bool Back = false;
-    [SerializeField] private bool Left = false;
-    [SerializeField] private bool Right = false;
+    [SerializeField] private bool _forward = false;
+    [SerializeField] private bool _back = false;
+    [SerializeField] private bool _left = false;
+    [SerializeField] private bool _right = false;
     private Vector3 _pointDirection;
 
     private void Start()
@@ -32,49 +30,23 @@ public class Spawner : MonoBehaviour
 
     public void SetDirectionEnemy()
     {
-        if (Forward == true & Right == false & Back == false & Left == false)
+        if (_forward == true & _right == false & _back == false & _left == false)
         {
             _pointDirection = Vector3.forward;
         }
-        else if(Forward == false & Right == true & Back == false & Left == false)
+        else if(_forward == false & _right == true & _back == false & _left == false)
         {
             _pointDirection = Vector3.right;
         }
-        else if(Forward == false & Right == false & Back == true & Left == false) 
+        else if(_forward == false & _right == false & _back == true & _left == false) 
         {
             _pointDirection = Vector3.back;
         }
-        else if(Forward == false & Right == false & Back == false & Left == true)
+        else if(_forward == false & _right == false & _back == false & _left == true)
         {
             _pointDirection = Vector3.left;
         }
     }
-
-    //public void SetDirection(string direction)
-    //{
-    //    switch (direction)
-    //    {
-    //        case FORWARD:
-    //            _pointDirection = Vector3.forward;
-    //            break;
-
-    //        case DOWN:
-    //            _pointDirection = Vector3.down;
-    //            break;
-
-    //        case BACK:
-    //            _pointDirection = Vector3.back;
-    //            break;
-
-    //        case LEFT:
-    //            _pointDirection = Vector3.left;
-    //            break;
-
-    //        default:
-    //            Console.WriteLine("Ошибка нет такого направоения");
-    //            break;
-    //    }
-    //}
 
     private IEnumerator Create()
     {
